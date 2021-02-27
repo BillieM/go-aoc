8cb84e38fa17ccaa53baacef8d1b77b005e22a36
BillieM <billiemerz@gmail.com>
2021-02-27 20:12:47 +0000

    day 7 problem 2

--- 2017/7/main.go
+++ 2017/7/main.go
@@ -12,6 +12,7 @@ import (
 type program struct {
 	name        string
 	weight      int
+	stackWeight int
 	childrenStr []string
 	children    []*program
 	parents     []*program
@@ -83,6 +84,29 @@ func childrenAndParents(p *program, structsMap map[string]*program) {
 	}
 }
 
+func getStackWeight(p *program) int {
+	childrenWeight := 0
+	childrenWeight += p.weight
+
+	for _, child := range p.children {
+		childrenWeight += getStackWeight(child)
+	}
+
+	return childrenWeight
+}
+
+func isBalanced(p *program) bool {
+
+	for _, a := range p.children {
+		for _, b := range p.children {
+			if a.weight != b.weight {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func problemOne(structsMap map[string]*program) {
 	/*
 		loop through structsMap, return name of the program with 0 parents
@@ -90,15 +114,43 @@ func problemOne(structsMap map[string]*program) {
 	counter := 0
 	for _, p := range structsMap {
 		if len(p.parents) == 0 {
-			fmt.Println(p)
 			counter++
 		}
 	}
-	fmt.Println(counter)
+}
+
+func problemTwo(structsMap map[string]*program) {
+	/*
+		for all nodes, need to check stack weight of all children is equal
+
+		common child between uneven stacks is problem node
+
+		stack weight calculated by recursively adding all children wt for each node
+
+	*/
+	for _, p := range structsMap {
+		p.stackWeight = getStackWeight(p)
+	}
+
+	var minP *program
+	minVal := 1000000
+
+	for _, p := range structsMap {
+		if !isBalanced(p) && p.stackWeight < minVal {
+			minP = p
+		}
+	}
+
+	_ = minP
+
+	for _, child := range structsMap["aobgmc"].children {
+		fmt.Println(child)
+	}
 }
 
 func main() {
 	lines := getInput()
 	structsMap := getStructsMap(lines)
-	problemOne(structsMap)
+	// problemOne(structsMap)
+	problemTwo(structsMap)
 }
