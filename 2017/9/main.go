fc9addaef84bf8f3bde758e18e6fc3c2a2830cc3
BillieM <billiemerz@gmail.com>
2021-03-02 21:35:45 +0000

    create folder/ file for day 9 and brief notes

---
+++ 2017/9/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+}
+
+/*
+we track the current group level
+
+if a group is broken, decrement the group level
+
+if a group is closed, increase score and decrement group level
+*/
