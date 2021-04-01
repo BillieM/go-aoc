51f4404ba2e97264185a2541371d785a103d4873
billiem <billiemerz@gmail.com>
2021-04-01 21:21:05 +0100

    day 11 solved

--- 2017/11/main.go
+++ 2017/11/main.go
@@ -29,39 +29,86 @@ func getInput() []string {
 	return instructions
 }
 
-func problemOne(instructions []string) {
-	var instructionMap = map[string]int{
-		"UD": 0, // up down
-		"LR": 0, // left right
-		"RD": 0, // right diagonal /
-		"LD": 0, // left diagonal \
-	}
+func problemOne(instructions []string) int {
+
+	var vert float64
+	var hori float64
+
+	var steps []string
 
 	for _, instruction := range instructions {
 		switch instruction {
 		case "n":
-			instructionMap["UD"]++
+			vert++
 		case "s":
-			instructionMap["UD"]--
-		case "e":
-			instructionMap["LR"]++
-		case "w":
-			instructionMap["LR"]--
+			vert--
 		case "ne":
-			instructionMap["RD"]++
-		case "sw":
-			instructionMap["RD"]--
+			vert += 0.5
+			hori++
 		case "nw":
-			instructionMap["LD"]++
+			vert += 0.5
+			hori--
 		case "se":
-			instructionMap["LD"]--
+			vert -= 0.5
+			hori++
+		case "sw":
+			vert -= 0.5
+			hori--
 		}
 	}
 
-	fmt.Println(instructionMap)
+	for vert != 0 {
+		for hori != 0 {
+			if hori > 0 {
+				if vert > 0 {
+					vert -= 0.5
+					steps = append(steps, "ne")
+				} else {
+					vert += 0.5
+					steps = append(steps, "se")
+				}
+				hori--
+			} else {
+				if vert > 0 {
+					vert -= 0.5
+					steps = append(steps, "nw")
+				} else {
+					vert += 0.5
+					steps = append(steps, "sw")
+				}
+				hori++
+			}
+		}
+
+		if vert > 0 {
+			vert--
+			steps = append(steps, "n")
+		} else if vert < 0 {
+			vert++
+			steps = append(steps, "s")
+		}
+	}
+
+	return len(steps)
+}
+
+func problemTwo(instructions []string) int {
+	// loop through all possible slices and calc num steps
+	maxSteps := 0
+
+	for i := 0; i < len(instructions); i++ {
+		slice := instructions[:i+1]
+		sliceSteps := problemOne(slice)
+		if sliceSteps > maxSteps {
+			maxSteps = sliceSteps
+		}
+	}
+
+	return maxSteps
 }
 
 func main() {
 	instructions := getInput()
-	problemOne(instructions)
+	// fmt.Println(problemOne(instructions))
+	fmt.Println(problemTwo(instructions))
 }
